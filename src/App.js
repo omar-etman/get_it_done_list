2d71f26645371649151292690b8fe2d3ae8aca49
omar-etman <omartareketman@gmail.com>
2021-12-27 15:29:19 +0200

    initialize the input component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import Input from './components/Input';
 
 function App() {
   return (
     <div className="App">
       <h1>Get It Done!</h1>
       <h2>taskmanaging todo's list</h2>
+      {/*todos list*/}
+      {/* done */}
+      <Input />
+
     </div>
   );
 }
